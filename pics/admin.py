d12481dc06a924cc6a41a59503b92e9d0d687f56
damunza <danidamunza@gmail.com>
2018-09-27 10:33:56 +0300

    created the models for the app

--- pics/admin.py
+++ pics/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+# from .models import Image, Places, Category
+#
+# # Register your models here.
+#
+# admin.site.register(Image)
+# admin.site.register(Places)
+# admin.site.register(Category)
 
-# Register your models here.
--- pics/models.py
+++ pics/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Places(models.Model):
+    location = models.CharField(max_length= 40)
+
+    def __str__(self):
+        return self.location
+
+class Category(models.Model):
+    category = models.CharField(max_length= 30)
+
+    def __str__(self):
+        return self.category
+
+class Image(models.Model):
+    name = models.CharField(max_length= 50)
+    description = models.TextField()
+    location = models.ForeignKey(Places)
+    category = models.ForeignKey(Category)
+    image = models.ImageField(upload_to='pictures/')
+
+    def __str__(self):
+        return self.name
+
